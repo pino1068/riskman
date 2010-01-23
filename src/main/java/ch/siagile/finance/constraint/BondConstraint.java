e3ad5a89724b496df826f807061f7423eb0c2348
Giuseppe Di Pierri <pino@pino.local>
2010-01-23 13:34:36 +0100

    moved IsBondMatcher from test to main

--- src/main/java/ch/siagile/finance/constraint/BondConstraint.java
+++ src/main/java/ch/siagile/finance/constraint/BondConstraint.java
@@ -4,6 +4,7 @@ import org.hamcrest.*;
 
 import ch.siagile.finance.*;
 import ch.siagile.finance.check.*;
+import ch.siagile.finance.matcher.*;
 import ch.siagile.finance.position.*;
 
 public class BondConstraint extends Constraint {
--- src/main/java/ch/siagile/finance/matcher/IsBondMatcher.java
+++ src/main/java/ch/siagile/finance/matcher/IsBondMatcher.java
@@ -1,4 +1,4 @@
-package ch.siagile.finance;
+package ch.siagile.finance.matcher;
 
 import org.hamcrest.*;
 
