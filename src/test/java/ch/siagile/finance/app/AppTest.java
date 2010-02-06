6731bef30e0f8a3924b3a708d4a93797487f42d7
Giuseppe Di Pierri <pino@pino.local>
2010-02-06 17:26:11 +0100

    big refactoring of command names, see help for details

--- src/test/java/ch/siagile/finance/app/AppTest.java
+++ src/test/java/ch/siagile/finance/app/AppTest.java
@@ -28,16 +28,14 @@ public class AppTest {
 		app.start();
 
 		check("Menu: --------------------");
-		check("	'h' or 'help'			- print this help");
-		check("	'p' or 'positions'		- print all positions");
-//		check("	'exe:<constraint>'		- to apply a constraint and get the result printed");
-//		check("	'exe:load:<file>'		- to load a file with positions and get warnings printed");
-		check("	'f:<criteria>'			- print positions filtered by given criteria");
-		check("	'split:<criteria>'		- print the splitted groups by given criteria");
-		check("	'check:<split>'		- split and check according to criteria and print results, i.e. splitby:owner max:5%");
-		check("	'r or remove'			- removes last filter");
-		check("	'q' or 'quit'			- to exit this program");
-		check("	'load:<file>'			- load positions from given file");
+		check("print this help");
+		check("print all positions");
+		check("print positions filtered by given criteria");
+		check("print the splitted groups by given criteria");
+		check("split and check according to criteria and print results, i.e. splitby:owner max:5%");
+		check("removes last filter");
+		check("to exit this program");
+		check("load positions from given file");
 		check("--------------------------");
 	}
 
